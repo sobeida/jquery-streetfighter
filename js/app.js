fb18423fc5d9209646687a2910204bd8432b2173
sobeida <sobeidalagrange7@gmail.com>
2017-02-24 18:29:28 -0500

    http
    to https.

---
+++ js/app.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $('.ryu-action').hide(); //(this line means hide all 4 divs)
+    $('.ryu-still').show();
+    $('.ryu').mouseenter(function () {
+            $('.ryu-action').hide();
+            $('.ryu-ready').show();
+        })
+        .mouseleave(function () {
+            $('.ryu-action').hide();
+            $('.ryu-still').show();
+
+        })
+        .mousedown(function () {
+            playHadouken();
+            $('.ryu-action').hide();
+            $('.ryu-throwing').show();
+            $('.hadouken').finish().show().animate({
+                    'left': '300px'
+                },
+                500,
+                function () {
+                    $(this).hide();
+                    $(this).css('left', '-212px');
+                });
+        })
+        .mouseup(function () {
+            $('.ryu-action').hide();
+            $('.ryu-ready').show();
+        });
+    /*on key press
+          —> keydown —> display ryu-cool (and hide everythying else)*/
+    $(document).keydown(function (key) {
+        //keyCode == 88 is the X key
+        if (key.keyCode == 88) {
+            $('.ryu-action').hide(); //(this line means hide all 4 divs)
+            $('.ryu-cool').show();
+            playCool()
+        }
+    });
+
+    /* —> keyup —> display the default state --> ryu-still (and hide everythying else)*/
+    $(document).keyup(function (key) {
+        if (key.keyCode == 88) {
+            $('.ryu-action').hide(); //(this line means hide all 4 divs)
+            $('.ryu-still').show();
+            $('#cool')[0].pause();
+        }
+    });
+});
+
+/*how to play a sound */
+var hadoukenSound = false;
+
+function playHadouken() {
+    hadoukenSound = !hadoukenSound;
+    if (hadoukenSound) {
+        $('#hadouken-sound')[0].volume = 0.5; //set the volume (0 => 0% and 1 => 100%; so 0.5 is 50%)
+        $('#hadouken-sound')[0].load(); //load the sound into the memory
+        $('#hadouken-sound')[0].play(); //play it
+    }
+}
+
+var coolSound = false;
+
+function playCool() {
+    coolSound = !coolSound;
+    if (coolSound) {
+        // $('#cool')[0].load()
+        $('#cool')[0].play();
+    }
+}
